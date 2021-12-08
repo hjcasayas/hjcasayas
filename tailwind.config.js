d7e7850cd0fdceef9b837f566aad566eb7767e3d
hjcasayas <henlyjade.casayas@gmail.com>
2021-12-09 04:10:53 +0800

    feat: extended default screens of tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   mode: "jit",
-  purge: ["src/pages/**/*.tsx"],
+  purge: ["src/pages/**/*.tsx", "src/components/**/*.tsx"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: "480px",
+        sm: "576px",
+        ...defaultTheme.screens,
+      },
+    },
   },
   variants: {
     extend: {},
