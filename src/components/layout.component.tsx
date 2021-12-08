8e8f833f35c4d71e323d7ae2682097be3314c4ea
hjcasayas <henlyjade.casayas@gmail.com>
2021-12-09 04:11:32 +0800

    feat: added layout component

---
+++ src/components/layout.component.tsx
@@ -0,0 +1,38 @@
+import { FC, ReactNode } from "react";
+import Head from "next/head";
+import Link from "next/link";
+
+interface LayoutComponentProps {
+  children: ReactNode;
+}
+
+export const LayoutComponent: FC<LayoutComponentProps> = ({ children }) => (
+  <>
+    <Head>
+      <meta
+        name="description"
+        content="Henly Jade Casayas' software developer portfolio."
+      />
+      <meta name="author" content="hjcasayas" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <div className="flex flex-col w-screen min-h-screen pt-5 px-6 bg-gray-100">
+      <div className="mx-auto max-w-5xl">
+        <header className="flex justify-between mb-16">
+          <div>
+            <h1 className="text-gray-700 text-2xl leading-7 font-semibold mb-1">
+              <Link href="/">
+                <a>Henly Jade Casayas</a>
+              </Link>
+            </h1>
+            <p className="text-gray-500 text-lg font-medium">
+              Software Developer
+            </p>
+          </div>
+        </header>
+        <main>{children}</main>
+        <footer></footer>
+      </div>
+    </div>
+  </>
+);
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
+import { LayoutComponent } from "../components/layout.component";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <LayoutComponent>
+      <Component {...pageProps} />
+    </LayoutComponent>
+  );
 }
 
 export default MyApp;
