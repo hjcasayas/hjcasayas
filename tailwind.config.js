e6b7c8a8424602baade51e04a1d931f943e0c71b
hjcasayas <henlyjade.casayas@gmail.com>
2021-12-28 16:56:56 +0800

    feat: removed additional screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,8 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const defaultTheme = require("tailwindcss/defaultTheme");
-
 module.exports = {
   mode: "jit",
   purge: ["src/pages/**/*.tsx", "src/components/**/*.tsx"],
   darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      screens: {
-        xs: "480px",
-        sm: "576px",
-        ...defaultTheme.screens,
-      },
-    },
-  },
+  theme: {},
   variants: {
     extend: {},
   },
