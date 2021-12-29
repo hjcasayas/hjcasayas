98a5a0df8d4a77ad24cd652e8a4a47200d288aaf
hjcasayas <henlyjade.casayas@gmail.com>
2021-12-30 00:20:29 +0800

    fix: use nextjs config for environment variables

--- next.config.js
+++ next.config.js
@@ -1,3 +1,25 @@
-module.exports = {
-  reactStrictMode: true,
-}
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+
+module.exports = (phase, { defaultConfig }) => {
+  /**
+   * @type {import('next').NextConfig}
+   */
+  let nextConfig = {
+    ...defaultConfig,
+    env: {
+      DEV_CHALLENGES_IO_BASE_URL: "http://dev-challenges.hjcasayas.com",
+    },
+  };
+
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    nextConfig = {
+      ...nextConfig,
+      env: {
+        DEV_CHALLENGES_IO_BASE_URL: "https://dev-challenges.hjcasayas.dev",
+      },
+    };
+  }
+
+  return nextConfig;
+};
