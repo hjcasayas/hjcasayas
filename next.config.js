ddf13b637dd21445f7ebe0c16b4c2a385673816e
hjcasayas <henlyjade.casayas@gmail.com>
2021-12-31 12:47:14 +0800

    feat: updated packages

--- next.config.js
+++ next.config.js
@@ -1,25 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
-
-module.exports = (phase, { defaultConfig }) => {
-  /**
-   * @type {import('next').NextConfig}
-   */
-  let nextConfig = {
-    ...defaultConfig,
-    env: {
-      DEV_CHALLENGES_IO_BASE_URL: "http://dev-challenges.hjcasayas.com",
-    },
-  };
-
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    nextConfig = {
-      ...nextConfig,
-      env: {
-        DEV_CHALLENGES_IO_BASE_URL: "https://dev-challenges.hjcasayas.dev",
-      },
-    };
-  }
-
-  return nextConfig;
+/** @type {import('next').NextConfig} */
+module.exports = {
+  reactStrictMode: true,
 };
