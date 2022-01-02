6e0dc44ea5bc45adf664ef2f1242bef26dbc60ac
hjcasayas <henlyjade.casayas@gmail.com>
2022-01-02 19:07:17 +0800

    feat: use Link instead of anchor tag alone

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,21 @@
 import { NextPage } from "next";
 import { ProjectCardComponent } from "../components/project-card.component";
 import { projectsList } from "../project/project.list";
+import Link from "next/link";
 
 const HomePage: NextPage = () => {
   return (
     <div className="mx-[-.75rem] flex flex-wrap">
       {projectsList.map((p) => (
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href={p.href}
-          key={p.src}
-          className="flex p-3 w-full sm:w-1/2 lg:w-1/3 cursor-pointer"
-        >
-          <ProjectCardComponent {...p} />
-        </a>
+        <Link href={p.href} key={p.src}>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            className="flex p-3 w-full sm:w-1/2 lg:w-1/3 cursor-pointer"
+          >
+            <ProjectCardComponent {...p} />
+          </a>
+        </Link>
       ))}
     </div>
   );
