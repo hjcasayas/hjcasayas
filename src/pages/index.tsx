dc47c2bd29f4de593888064f99b0b5221488e3bc
hjcasayas <henlyjade.casayas@gmail.com>
2022-05-05 23:50:35 +0800

    feat: remove Link tag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,20 @@
 import { NextPage } from "next";
 import { ProjectCardComponent } from "../components/project-card.component";
 import { projectsList } from "../project/project.list";
-import Link from "next/link";
 
 const HomePage: NextPage = () => {
   return (
     <div className="mx-[-.75rem] flex flex-wrap">
       {projectsList.map((p, index) => (
-        <Link href={p.href} key={p.src}>
-          <a
-            target="_blank"
-            rel="noreferrer"
-            className="flex p-3 w-full sm:w-1/2 lg:w-1/3 cursor-pointer"
-          >
-            <ProjectCardComponent {...p} index={index} />
-          </a>
-        </Link>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          className="flex p-3 w-full sm:w-1/2 lg:w-1/3 cursor-pointer"
+          key={p.src}
+          href={p.href}
+        >
+          <ProjectCardComponent {...p} index={index} />
+        </a>
       ))}
     </div>
   );
